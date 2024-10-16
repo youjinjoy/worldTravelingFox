0eb72ff8c5cd2b1ecac610c83b5bdb95bb87d052
Youjin Jo <youjinjoy@gmail.com>
2024-10-16 23:44:42 +0900

    completed overlay loading

--- src/Experience/Loading/Loading.js
+++ src/Experience/Loading/Loading.js
@@ -1,8 +1,9 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { gsap } from 'gsap'
+
 /**
  * Overlay
  */
-
 const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
 const overlayMaterial = new THREE.ShaderMaterial({
     transparent: true,
@@ -24,7 +25,22 @@ const overlayMaterial = new THREE.ShaderMaterial({
             gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha );
         }
     `
-
 })
+
+/**
+ * Loaders
+ */
+export const loadingManager = new THREE.LoadingManager(
+    // Loaded
+    () => {
+        gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0 })
+    },
+
+    // Progress
+    () => {
+        console.log('progress')
+    }
+)
+
 
 export const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import EventEmitter from './EventEmitter.js'
 
-import { loadingManager } from '../Loading/LoadingManager.js'
+import { loadingManager } from '../Loading/Loading.js'
 
 export default class Resources extends EventEmitter
 {
